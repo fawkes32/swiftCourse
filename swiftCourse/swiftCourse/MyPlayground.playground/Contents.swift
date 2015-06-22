4fb7480c470c01aa80ae60a9718856c33a7dba8a
Daniel Márquez <daniel.marquez89@gmail.com>
2015-06-22 12:02:49 -0500

    primeros ejercicios en Swift utilizando el playground.

---
+++ swiftCourse/swiftCourse/MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+
+println("Hola Mundo")
+var variable = "un valor"
+var variable2:String = "un string"
+
+let constante = "esta es una constante"
+let constante2 : String = " esta es otra constante"
+
+var flotante : Double = 2.09090911111112
+
+var entero = 1
+var string = String(entero)
+var doble:Double = Double(entero)
+
+var cadena  = "1.5"
+
+var parse = (cadena as NSString).doubleValue
