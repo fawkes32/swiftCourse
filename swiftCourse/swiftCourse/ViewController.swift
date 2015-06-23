6683ef4561d6e66bf0f8778d72cc7e30601b56ef
Daniel Márquez <daniel.marquez89@gmail.com>
2015-06-23 12:00:52 -0500

    App inicial Saludo
    desarrollo de la primera app, estilo hello world

--- swiftCourse/swiftCourse/ViewController.swift
+++ swiftCourse/swiftCourse/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtNombre: UITextField!
+    
+    @IBOutlet weak var lblResultado: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func actionSaludo(sender: AnyObject) {
+        lblResultado.text = "Hola Amigo " + txtNombre.text!
+        
+    }
 
 }
 
