3fe24a4d1f7e9e5b65d0120a6deb20907c92bc2b
Daniel Márquez <daniel.marquez89@gmail.com>
2015-06-23 09:19:09 -0500

    Antes de la segunda Clase

--- swiftCourse/swiftCourse/MyPlayground.playground/Contents.swift
+++ swiftCourse/swiftCourse/MyPlayground.playground/Contents.swift
@@ -72,7 +72,7 @@ default:
 
 
 
-var nombres = ["daniel","Juan", "Rafael", "Paco"]
+var nombres = ["Daniel","Juan", "Rafael", "Paco"]
 
 for (var i=0; i < nombres.count; i++){
     
@@ -81,12 +81,135 @@ for (var i=0; i < nombres.count; i++){
 }
 
 
+for nombre in nombres{
+    println("Hola \(nombre)")
+}
+
+for i in 0..<4{
+    println(nombres[i])
+}
+
+
+var i = 0
+while i < 4{
+    println("Ciclo \(i)")
+    i++ //comentar esta linea para que el ciclo se realice de manera infinita.
+    
+}
 
 
 
+i = 0
+do{
+    println("cliclo \(i)")
+    i++
+}while(i < 10)
 
 
+//Funciones
 
+
+func saluda(){
+    println("Hola")
+}
+
+
+saluda()
+
+func saludaA(nombre:String, apellido: String, edad: Int){
+    
+    println("Hola \(nombre) \(apellido) de \(edad)")
+    
+}
+
+
+saludaA("Daniel", "Marquez", 26)
+
+
+func suma (numero1: Int, numero2: Int)->Int{
+    
+    return numero1+numero2
+    
+}
+
+var z = suma(2,3)
+
+
+func operaciones(numero1 : Int, numero2 :Int) -> (suma:Int, resta:Int, multiplicacion: Int, division:Float) {
+    
+    var suma = numero1 + numero2
+    var resta = numero1 - numero2
+    var multiplicacion = numero1*numero2
+    var division:Float = Float(numero1)/Float(numero2)
+    return (suma, resta, multiplicacion, division)
+}
+
+var suma = 0
+var resta = 0
+var multiplicacion = 0
+var division:Float = 0
+
+var resultadoOperaciones = operaciones(3, 2)
+suma = operaciones(3,2).suma
+resta = resultadoOperaciones.resta
+println(suma)
+
+//mas de funciones
+
+func sumaDe(numeros: Int...) -> Int{
+    var suma = 0
+    for numero in numeros{
+        suma += numero
+    }
+    return suma
+}
+
+sumaDe(2,43,56)
+
+
+func perimetro(numeroLados:Int, longLados:Double) -> Double{
+    
+    
+    var resultado:Double = 0
+    
+    if numeroLados > 2 && numeroLados > 0{
+    
+        for var i = 0; i < numeroLados; i++ {
+            
+            resultado = resultado + longLados
+    
+        }
+    
+    
+        return resultado
+    } else {
+        println("no es posible, lados son menos de 2")
+        return resultado
+    }
+    
+}
+
+
+func perimetro(longLados:Double...) -> Double{
+    
+    var resultado:Double = 0
+    if longLados.count > 2 {
+    
+        for long in longLados {
+        
+            resultado += long
+    
+        }
+    
+        return resultado
+        
+    } else{
+        return resultado
+    }
+}
+
+
+perimetro(3,4,5,6,1)
 
 
 
