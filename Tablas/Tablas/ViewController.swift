62cd0dc577f14f00c821213bed7f2bd424da3158
Daniel Márquez <daniel.marquez89@gmail.com>
2015-06-24 10:18:47 -0500

    Tablas
    Ejercicio de Tablas

---
+++ Tablas/Tablas/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Tablas
+//
+//  Created by Daniel Marquez on 6/24/15.
+//  Copyright (c) 2015 Tec de Monterrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    var frutas = ["Mango", "Melon", "Fresa"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return frutas.count
+        
+    }
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        
+        let cell:UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "cell")
+        cell.textLabel!.text = frutas[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    
+    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        println("\(indexPath.row)")
+    }
+
+
+}
+
